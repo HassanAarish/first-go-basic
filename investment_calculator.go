ad103dd62f2abc1b236832f0f93f85eaf60d5640
Aarish Jafri <aarishhassan98@gmail.com>
2025-09-02 01:24:02 +0500

    learned about more ways to work with variables and expressionsn and take users input for the values and how we can access tthe exact location of the point using abasent sign with fmt.scan() function. Complete investment calculator withh users inputs.

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,10 +8,19 @@ import (
 func main() {
 	fmt.Println("Starting program ....")
 
-	const inflationRate = 5.5
-	var investmentAmount float64 = 1000
-	expectedReturnRate := 5.5
-	noOfYears:= 10.0
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var expectedReturnRate float64
+	noOfYears := 10.0
+
+	fmt.Print("Please enter the amount you wish to invest ==> ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Please enter the expected return rate ==> ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Please enter the no. of years you to wish invest for ==> ")
+	fmt.Scan(&noOfYears)
 
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100,noOfYears)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, noOfYears)
