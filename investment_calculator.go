2e0368ce431717a55af7557c485312cd339e8d4f
Aarish Jafri <aarishhassan98@gmail.com>
2025-09-02 01:01:01 +0500

    learned about more ways to work with variables and expressionsn.

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,24 +8,17 @@ import (
 func main() {
 	fmt.Println("Starting program ....")
 
-	// Different ways to use the values an there types depending on the requirements.
-
-	// investmentAmount, noOfYears, expectedReturnRate := 1000.00, 10.00, 5.5
-	// investmentAmount:= 1000.00
-	// expectedReturnRate :=10.00
-	// noOfYears:= 5.5
-
-	// var investmentAmount float64 = 1000
-	// var expectedReturnRate  float64 =10
-	// var noOfYears float64 = 5.5
-
+	const inflationRate = 5.5
 	var investmentAmount float64 = 1000
-	expectedReturnRate :=10.00
-	noOfYears:= 5.5
+	expectedReturnRate := 5.5
+	noOfYears:= 10.0
 
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100,noOfYears)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, noOfYears)
 
 	fmt.Println("Future Value ===>", futureValue)
+
+	fmt.Println("Future Real Value ===>", futureRealValue)
 
 	fmt.Print("Program ended successfully !")
 }
