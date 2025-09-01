dae3e4b3a1ecdf4849f462d7e206e472a4f11330
Aarish Jafri <aarishhassan98@gmail.com>
2025-09-02 00:51:04 +0500

    learned about different ways to declare variables and there types as per requirements.

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,9 +8,16 @@ import (
 func main() {
 	fmt.Println("Starting program ....")
 
+	// Different ways to use the values an there types depending on the requirements.
+
+	// investmentAmount, noOfYears, expectedReturnRate := 1000.00, 10.00, 5.5
+	// investmentAmount:= 1000.00
+	// expectedReturnRate :=10.00
+	// noOfYears:= 5.5
+
 	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var noOfYears float64 = 10
+	var expectedReturnRate  float64 =10
+	var noOfYears float64 = 5.5
 
 	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate/100,noOfYears)
 
