c7222f4a2cb8097bea09a06a58a2f52892e8e646
Aarish Jafri <aarishhassan98@gmail.com>
2025-09-06 22:52:52 +0500

    feat: Learned new way to return the values and also how these values can be store in the variable.

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,25 +5,31 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
 	fmt.Println("Starting program ....")
 
-	const inflationRate = 2.5
 	var investmentAmount float64
-	var expectedReturnRate float64
-	noOfYears := 10.0
+	var noOfYears float64
+	expectedReturnRate := 5.5
 
-	fmt.Print("Please enter the amount you wish to invest ==> ")
+	// fmt.Print("Please enter the amount you wish to invest ==> ")
+	outputText(`Please enter the amount you wish to invest ==> `)
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Please enter the expected return rate ==> ")
-	fmt.Scan(&expectedReturnRate)
-
-	fmt.Print("Please enter the no. of years you to wish invest for ==> ")
+	// fmt.Print("Please enter the no. of years you to wish invest for ==> ")
+	outputText("Please enter the no. of years you to wish invest for ==> ")
 	fmt.Scan(&noOfYears)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, noOfYears)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, noOfYears)
+	// fmt.Print("Please enter the expected return rate ==> ")
+	outputText("Please enter the expected return rate ==> ")
+	fmt.Scan(&expectedReturnRate)
+
+	futureValue, futureRealValue := calulateFutureValue(investmentAmount, expectedReturnRate, noOfYears)
+
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, noOfYears)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, noOfYears)
 
 	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
 	formattedRFV := fmt.Sprintf("Future Real Value: %.2f\n", futureRealValue)
@@ -39,4 +45,14 @@ func main() {
 	// fmt.Println("Future Real Value ===>", futureRealValue)
 
 	fmt.Print("Program ended successfully !")
+}
+
+func outputText(value string) {
+	fmt.Print(value)
+}
+
+func calulateFutureValue(investmentAmount, expectedReturnRate, noOfYears float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, noOfYears)
+	rfv := fv / math.Pow(1+inflationRate/100, noOfYears)
+	return fv, rfv
 }
