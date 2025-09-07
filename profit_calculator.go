c8d17300a9f8ab5f5d77f31e69d52814e9596344
Aarish Jafri <aarishhassan98@gmail.com>
2025-09-07 22:46:54 +0500

    feat: Implemented new methods to write and read the system files from the course.

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -11,19 +13,37 @@ func main() {
 	// var expenses float64
 	// var taxRate float64
 
-	revenue := getUserInput("Revenue: ")
+	revenue, err1 := getUserInput("Revenue: ")
+	// if err1 != nil {
+	// 	fmt.Println("Error found: ", err1)
+	// 	return
+	// }
 	// fmt.Print("Revenue: ")
 	// fmt.Scan(&revenue)
 
-	expenses := getUserInput("Expenses: ")
+	expenses, err2 := getUserInput("Expenses: ")
+	// if err2 != nil {
+	// 	fmt.Println("Error found: ", err2)
+	// 	return
+	// }
 	// fmt.Print("Expenses: ")
 	// fmt.Scan(&expenses)
 
-	taxRate := getUserInput("Tax Rate: ")
+	taxRate, err3 := getUserInput("Tax Rate: ")
+	// if err3 != nil {
+	// 	fmt.Println("Error found: ", err3)
+	// 	return
+	// }
 	// fmt.Print("Tax Rate: ")
 	// fmt.Scan(&taxRate)
 
+	if err1 != nil || err2 != nil || err3 != nil {
+		fmt.Println("Error found: ", err1)
+		return
+	}
+
 	ebt, profit, ratio := calulateValues(revenue, expenses, taxRate)
+	writeCalulatedData(ebt, profit, ratio)
 	// ebt := revenue - expenses
 	// profit := ebt * (1 - taxRate/100)
 	// ratio := ebt / profit
@@ -37,11 +57,14 @@ func main() {
 	fmt.Print("Profit Calculated successfully !")
 }
 
-func getUserInput(text string) float64 {
+func getUserInput(text string) (float64, error) {
 	var value float64
 	fmt.Print(text)
 	fmt.Scan(&value)
-	return value
+	if value <= 0 {
+		return 0, errors.New("Error, value can not be less then 0.")
+	}
+	return value, nil
 }
 
 func calulateValues(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64) {
@@ -50,4 +73,9 @@ func calulateValues(revenue, expenses, taxRate float64) (ebt float64, profit flo
 	ratio = ebt / profit
 
 	return ebt, profit, ratio
+}
+
+func writeCalulatedData(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.2f \n Profit: %.2f \n Ratio: %.2f \n", ebt, profit, ratio)
+	os.WriteFile("data.txt", []byte(results), 0664)
 }
